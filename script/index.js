f1dbcec2d82c3c7e06d486b5b93e9cbf1318676e
Your Name <you@example.com>
2026-03-10 05:02:24 +0600

    some meaning full changes my english vocavulary

--- script/index.js
+++ script/index.js
@@ -1,9 +1,35 @@
 
-const lessons = () =>{
+const levellessons = () =>{
     fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
     .then(json => displayLesson(json.data))
+};
+const loadlevelword = (id)=>{
+     const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayShowWord(data.data))
 }
+const displayShowWord =(words)=>{
+    
+    const wordContainer =document.getElementById('word-container')
+    wordContainer.innerHTML = "";
+    words.forEach(word => {
+        
+console.log(word)
+    
+    const card =document.createElement("div")
+    card.innerHTML =`
+    <p>cat</p>
+    `;
+    wordContainer.append(card)
+    
+   });
+
+
+}
+
+
 const displayLesson =(lessons)=>{
 //01. get the element 
 const levelContainer = document.getElementById('level-container');
@@ -13,13 +39,13 @@ levelContainer.innerHTML = "";
 for(let lesson of lessons){
     const btndiv = document.createElement("div");
     btndiv.innerHTML=`
-    <button>
-      <i  class="fa-solid fa-book-open-reader"></i>Lesson
+    <button onclick="loadlevelword(${lesson.level_no})" class ="btn btn-outline btn-primary">
+      <i class="fa-solid fa-book-open-reader"></i>Lesson-${lesson.level_no}
       </button>
 
 
     `
     levelContainer.append(btndiv)
-}
+}}
 
-lessons();
+levellessons();
